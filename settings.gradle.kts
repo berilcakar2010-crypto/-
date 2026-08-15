0a1ee612d5abf3803133b046b8748e3e83969b61
berilcakar2010-crypto <beril.cakar2010@gmail.com>
2026-08-15 17:32:17 +0300

    Fix: Remove unavailable kotlin.plugin.compose plugin declaration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,6 @@ dependencyResolutionManagement {
 plugins {
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.24" apply false
 }
 
 rootProject.name = "kaomoji"
