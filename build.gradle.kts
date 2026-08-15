a9093ef08f91291d82ca81050a898c9f74fe9250
berilcakar2010-crypto <beril.cakar2010@gmail.com>
2026-08-15 17:24:12 +0300

    Change Android application plugin version
    Updated Android application plugin version from 8.5.2 to 8.3.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,9 @@ pluginManagement {
 }
 
 plugins {
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.24" apply false
-    // Diğer plugin tanımların buradadır...
-}
-// Kök proje build dosyası. Modüllere ortak plugin sürümlerini bildirir,
-// başka hiçbir şey yapmaz.
-plugins {
-    id("com.android.application") version "8.5.2" apply false
+    id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "1.9.24" apply false
 }
+
+true
