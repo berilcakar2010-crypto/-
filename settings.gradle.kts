9d09399b2da3e8dd2b4366227a4c1a95810b4f02
berilcakar2010-crypto <beril.cakar2010@gmail.com>
2026-08-15 17:39:58 +0300

    Fix: Downgrade Kotlin from 1.9.24 to 1.9.23 for Compose compatibility

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ dependencyResolutionManagement {
 
 plugins {
     id("com.android.application") version "8.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }
 
 rootProject.name = "kaomoji"
